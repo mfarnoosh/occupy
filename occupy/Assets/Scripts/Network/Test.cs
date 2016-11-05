6777560d6c0b5e6569af0c54ed938d38e6140d6b
Mehrdad <m.h.farnoosh88@gmail.com>
2016-11-06 00:56:26 +0330

    initial commit

--- occupy/Assets/Scripts/Network/Test.cs
+++ occupy/Assets/Scripts/Network/Test.cs
@@ -2,22 +2,25 @@
 using UnityEngine.UI;
 using System.Collections;
 
-public class Test : MonoBehaviour {
+public class Test : MonoBehaviour
+{
 	// Use this for initialization
-	void Start () {
-		NetworkManager.Current.SendToServer ("Salam").OnSuccess ((data)=>{
-			Debug.Log(data.value);
+	void Start ()
+	{
+		NetworkManager.Current.SendToServer ("Salam").OnSuccess ((data) => {
+			Debug.Log (data.value);
 
-			var go = GetComponent<Text>();
+			var go = GetComponent<Text> ();
 
-			if(go != null){
-				go.text=data.value;
+			if (go != null) {
+				go.text = data.value;
 			}
 		});
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+	{
 	
 	}
 }
