2a66ba73a825e957cdfe3f96169e2ec380a6caad
cross <alirezaght@gmail.com>
2016-12-01 21:05:25 +0330

    onScreen buttons

---
+++ occupy/Assets/Scripts/Interactions/GameObjects.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameObjects {
+
+	public class Building {
+		public int type = -1;
+	}
+	public class Unit {
+		public int type = -1;
+	}
+}
+
