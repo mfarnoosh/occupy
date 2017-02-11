e614f5c5be2a5c639151681f6517e424f2ac96d5
cross <alirezaght@gmail.com>
2017-02-11 20:37:49 +0330

    my location

--- occupy/Assets/Scripts/HUD/UiActions/MyLocation.cs
+++ occupy/Assets/Scripts/HUD/UiActions/MyLocation.cs
@@ -1,11 +1,20 @@
 ﻿using UnityEngine;
 using System.Collections;
-
+using System;
 public class MyLocation : EventAction {
 
 	public override void PointerClick ()
 	{
 		Debug.Log ("My Location Clicked.");
+		GPS gps = new GPS ();
+		StartCoroutine (gps.GetLocation (new Action<Location>(loc => {
+			Debug.Log ("My Location finished");
+			if (loc != null) {
+				Debug.Log (gps.location);
+				MapManager.Current.MoveTo (gps.location);
+			}
+		})));						
+			
 	}
 
 	public override void PointerDown (Vector2 position) { }
--- occupy/Assets/Scripts/Network/GPS.cs
+++ occupy/Assets/Scripts/Network/GPS.cs
@@ -1,27 +1,24 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System;
-
+using UnityToolbag;
 public class GPS : MonoBehaviour {
-	private float lat = 0;
-	private float lon = 0;
-	private float accuracy = 0;
-	private float alt = 0;
-	private double timeStamp = 0;
-	// Use this for initialization
-	IEnumerator Start () {
-		// First, check if user has location service enabled
-		long d = new DateTime (1970, 1, 1).Ticks;
-		if (!Input.location.isEnabledByUser)
-			yield break;
-		
-		// Start service before querying location
-		Input.location.Start();
+	public Location location = null;
+	public bool dataReady = false;
+	public IEnumerator GetLocation(Action<Location> callback) {		
 
-		// Wait until service initializes
+		if (!Input.location.isEnabledByUser) {
+			location = null;
+			dataReady = true;
+			print("not enabled");
+			yield break;
+		}
+		print("starting...");
+		Input.location.Start();
 		int maxWait = 20;
 		while (Input.location.status == LocationServiceStatus.Initializing && maxWait > 0)
 		{
+			print("waiting...");
 			yield return new WaitForSeconds(1);
 			maxWait--;
 		}
@@ -30,47 +27,37 @@ public class GPS : MonoBehaviour {
 		if (maxWait < 1)
 		{
 			print("Timed out");
+			location = null;	
+			dataReady = true;
 			yield break;
 		}
-
-
-
-
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		// Connection has failed
-		if (Input.location.status == LocationServiceStatus.Failed)
-		{
-			print("Unable to determine device location");
-		}
-		else
-		{
+		if (Input.location.status == LocationServiceStatus.Failed) {
+			print ("Unable to determine device location");
+			location = null;	
+			dataReady = true;
+			yield break;
+		} else {
 			// Access granted and location value could be retrieved
-			if (lat != Input.location.lastData.latitude || lon != Input.location.lastData.longitude) {
-				lat = Input.location.lastData.latitude;  
-				lon = Input.location.lastData.longitude;
-				alt = Input.location.lastData.altitude;
-				accuracy = Input.location.lastData.horizontalAccuracy;
-				timeStamp = Input.location.lastData.timestamp;
-				SocketMessage sm = new SocketMessage ();
-				sm.Cmd = "getTile";
-				sm.Params.Add (lat.ToString());
-				sm.Params.Add (lon.ToString());
-//				sm.Params.Add (alt.ToString());
-//				sm.Params.Add (accuracy.ToString());
-//				sm.Params.Add (timeStamp.ToString());
-				NetworkManager.Current.SendToServer (sm).OnSuccess((data)=>{
-					string tileString = data.value.Params[0];
-					byte[] tile = Convert.FromBase64String(tileString);
-					Texture2D texture = new Texture2D(256, 256);
-					texture.LoadImage(tile);
-					// do whatever you want with texture
-					String debug = "";
-				});
-			}
+			float lat = 0;
+			float lon = 0;
+			float accuracy = 0;
+			float alt = 0;
+			double timeStamp = 0;
+			lat = Input.location.lastData.latitude;  
+			lon = Input.location.lastData.longitude;
+			alt = Input.location.lastData.altitude;
+			accuracy = Input.location.lastData.horizontalAccuracy;
+			timeStamp = Input.location.lastData.timestamp;
+			print("lat = " + lat + " lon = " + lon);
+			location = new Location(lat, lon);
+			dataReady = true;
+			callback (location);
+			yield break;
 
-		}
+		}			
+		location = null;		
+		dataReady = true;
+		yield break;
 	}
+
 }
